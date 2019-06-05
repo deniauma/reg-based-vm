c62c40ad54981b11e472e7008b3216d07ca20d83
mdeniaud <mathieu.deniaud@crossknowledge.com>
2019-06-05 10:42:40 +0200

    Add regex for TokenType

--- src/lexer/mod.rs
+++ src/lexer/mod.rs
@@ -8,16 +8,32 @@ pub enum TokenType {
     Opcode,
     Register,
     IntegerOperand,
-    Number
 }
 
-pub type ResultToken = (TokenType, &'static str);
 
+#[derive(Debug)]
+pub struct TokenTypeRegex {
+    pub token_type: TokenType,
+    pub regex: Regex
+}
+
+impl TokenTypeRegex {
+    pub fn new(t: TokenType, re: &str) -> Self {
+        Self {
+            token_type: t,
+            regex: Regex::new(re).unwrap()
+        }
+    }
+}
+
+
+#[derive(Debug, PartialEq, Copy, Clone)]
 pub enum Token {
     Opcode(instruction::Opcode),
     Register(u8),
     IntegerOperand(i32)
 }
+
 
 pub struct AssemblerInstruction {
     opcode: Token,
@@ -26,40 +42,42 @@ pub struct AssemblerInstruction {
     arg3: Option<Token>,
 }
 
+
 pub struct Grammar {
     pub rules: HashMap<String, TokenType>,
+    pub terminal_rules: Vec<TokenTypeRegex>
 }
 
 impl Grammar {
     pub fn new() -> Self {
         Self {
             rules: HashMap::new(),
+            terminal_rules: vec!()
         }
     }
 
     pub fn add_rule(&mut self, src: &str, token_type: TokenType) {
         self.rules.insert(src.to_string(), token_type);
+        self.terminal_rules.push(TokenTypeRegex::new(token_type, src));
     }
 
-    pub fn parse_str(&self, src: &'static str) -> Option<ResultToken> {
-        for (s, token) in self.rules.iter() {
-            let re = Regex::new(s.as_str()).unwrap();
-            if re.is_match(src) {
-                println!("Found!");
-                match token {
+    pub fn parse_str(&self, src: &str) -> Option<Token> {
+        for t in &self.terminal_rules {
+            if t.regex.is_match(src) {
+                match t.token_type {
                     TokenType::Opcode => {
-                        let t = Token::Opcode(instruction::Opcode::from(src));
-                        return Some((token.clone(), src))
+                        let opcode = t.regex.captures(src).unwrap().name("op").unwrap().as_str();
+                        let op = Token::Opcode(instruction::Opcode::from(opcode));
+                        return Some(op)
                     },
                     TokenType::Register => {
-                        return Some((token.clone(), src.split_at(1).1))
+                        let n: u8 = t.regex.captures(src).unwrap().name("reg").unwrap().as_str().parse().unwrap();
+                        return Some(Token::Register(n))
                     },
                     TokenType::IntegerOperand => {
-                        return Some((token.clone(), src.split_at(1).1))
+                        let i: i32 = t.regex.captures(src).unwrap().name("intop").unwrap().as_str().parse().unwrap();
+                        return Some(Token::IntegerOperand(i))
                     },
-                    TokenType::Number => {
-                        return Some((token.clone(), src))
-                    }
                 }
             }
         }
@@ -69,8 +87,9 @@ impl Grammar {
 
 pub fn build_grammar() -> Grammar {
     let mut grammar = Grammar::new();
-    grammar.add_rule(r"^[a-z]+$", TokenType::Opcode);
-    grammar.add_rule(r"\$\d{1,2}", TokenType::Register);
+    grammar.add_rule(r"(?P<op>[a-z]+)", TokenType::Opcode);
+    grammar.add_rule(r"\$(?P<reg>\d{1,2})", TokenType::Register);
+    grammar.add_rule(r"\#(?P<intop>\d+)", TokenType::IntegerOperand);
     grammar 
 }
 
@@ -81,7 +100,21 @@ mod tests {
     #[test]
     fn test_opcode_load() {
         let grammar = build_grammar();
-        assert_eq!(grammar.parse_str("load").unwrap(), (TokenType::Opcode, "load"));
-        assert_eq!(grammar.parse_str("asz123"), None);
+        assert_eq!(grammar.parse_str("load"), Some(Token::Opcode(instruction::Opcode::LOAD)));
+        assert_eq!(grammar.parse_str("123"), None);
+    }
+
+    #[test]
+    fn test_register() {
+        let grammar = build_grammar();
+        assert_eq!(grammar.parse_str("$1"), Some(Token::Register(1)));
+        assert_eq!(grammar.parse_str("$"), None);
+    }
+
+    #[test]
+    fn test_integer_operand() {
+        let grammar = build_grammar();
+        assert_eq!(grammar.parse_str("#100"), Some(Token::IntegerOperand(100)));
+        assert_eq!(grammar.parse_str("#"), None);
     }
 }
